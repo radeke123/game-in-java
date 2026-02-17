8da6eab99d9a62ab50bfac68ed5b87fbef453131
radeke123 <radek.zapora15@gmail.com>
2026-02-17 16:38:56 +0100

    fg

--- build.bat
+++ build.bat
@@ -14,16 +14,34 @@ goto end
 :test
 javac -d test src/*.java
 jar cfm test/test-game.jar manifest.txt -C test .
+
+del /Q test\*.class
+
+echo @echo off > test/run.bat
+echo java -jar test-game.jar >> test/run.bat
+echo pause >> test/run.bat
 goto end
 
 :debug
 javac -d debug src/*.java
 jar cfm debug/debug-game.jar manifest.txt -C debug .
+
+del /Q debug\*.class
+
+echo @echo off > debug/run.bat
+echo java -jar debug-game.jar >> debug/run.bat
+echo pause >> debug/run.bat
 goto end
 
 :release
 javac -d release src/*.java
 jar cfm release/game.jar manifest.txt -C release .
+
+del /Q release\*.class
+
+echo @echo off > release/run.bat
+echo java -jar game.jar >> release/run.bat
+echo pause >> release/run.bat
 goto end
 
 :end
